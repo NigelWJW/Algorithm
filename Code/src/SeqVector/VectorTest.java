cecb5e773146ada6a1ec15e907515d199a2f6f51
NigelWJW <1341213521@qq.com>
2020-03-17 16:29:32 +0800

    2020.3.17 线性表修改及测试文件

---
+++ Code/src/SeqVector/VectorTest.java
@@ -0,0 +1,28 @@
+package SeqVector;
+
+public class VectorTest {
+
+    public static void main(String[] args){
+        Vector v = new Vector();
+        for (int i=0;i<10;i++){
+            v.insert(i,i);
+            v.size++;
+        }
+
+        System.out.println("size: "+v.size);
+        v.print(v.elem);
+
+        v.insert(10,5);
+        v.print(v.elem);
+
+        System.out.println(v.find(3,0,19));
+
+        v.remove(1,4);
+        v.print(v.elem);
+
+
+
+
+
+    }
+}
