6ee257a72498a83c0e2752a3f4627117e96c26b3
电子科大 <3046190668@qq.com>
2020-07-06 14:18:08 +0800

    2020.6.21 更新排序

--- Code/src/Sort/QuickSort.java
+++ Code/src/Sort/QuickSort.java
@@ -1,84 +1,37 @@
 package Sort;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 /**
  * 快速排序
  */
 public class QuickSort {
-        public static void main(String[] args) {
-            //int[] arr = {-9,78,0,23,-567,70, -1,900, 4561};
-            //测试快排的执行速度
-            // 创建要给80000个的随机的数组
-            int[] arr = new int[8000000];
-            for (int i = 0; i < 8000000; i++) {
-                arr[i] = (int) (Math.random() * 8000000); // 生成一个[0, 8000000) 数
-            }
-
-            System.out.println("排序前");
-            Date data1 = new Date();
-            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            String date1Str = simpleDateFormat.format(data1);
-            System.out.println("排序前的时间是=" + date1Str);
-
-            quickSort(arr, 0, arr.length-1);
-
-            Date data2 = new Date();
-            String date2Str = simpleDateFormat.format(data2);
-            System.out.println("排序前的时间是=" + date2Str);
-            //System.out.println("arr=" + Arrays.toString(arr));
+    private static void quicksort(int[] a,int lo,int hi){
+        if(lo>hi){
+            return;
         }
-        //快速排序
-        public static void quickSort(int[] arr,int left, int right) {
-            int l = left; //左元素下标
-            int r = right; //右元素下标
-            //pivot 这里中轴值取中间值，也可以改进其他中轴
-            int pivot = arr[(left + right) / 2];
-            int temp = 0; //临时变量，作为交换时使用
-            //while循环的目的是让比pivot 值小放到左边
-            //比pivot 值大放到右边
-            while( l < r) {//当左右游标合适的时候
-                //在pivot的左边一直找,找到大于等于pivot值,才退出
-                while(arr[l] < pivot) {
-                    l += 1;
-                }
-                //在pivot的右边一直找,找到小于等于pivot值,才退出
-                while(arr[r] > pivot) {
-                    r -= 1;
-                }
-                //如果l >= r说明pivot 的左右两的值，已经按照左边全部是
-                //小于等于pivot值，右边全部是大于等于pivot值
-                if( l >= r) {//退出条件
-                    break;//游标碰撞退出
-                }
-                //如果if后没有结束说明都找到了逆序元素，那么交换这两个元素
-                temp = arr[l];
-                arr[l] = arr[r];
-                arr[r] = temp;
-
-                //如果交换完后发现这个arr[l] == pivot值相等 r--， 前移
-                if(arr[l] == pivot) {
-                    r -= 1;
-                }
-                //如果交换完后，发现这个arr[r] == pivot值 相等 l++， 后移
-                if(arr[r] == pivot) {
-                    l += 1;
-                }
+        int base = a[lo];
+        int i = lo,j = hi;
+        while(i!=j){
+            while(a[j] >=base && i<j){
+                j--;
             }
-
-            // 如果游标重合 l == r, 必须l++, r--, 否则为出现栈溢出
-            if (l == r) {
-                l += 1;
-                r -= 1;
+            while(a[i] <= base && i<j){
+                i++;
             }
-
-            //向左子数列递归
-            if(left < r) {
-                quickSort(arr, left, r);
+            if(i<j){
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
             }
-            //向右子数列递归
-            if(right > l) {
-                quickSort(arr, l, right);
-            }
+        }
+        a[lo] = a[i];
+        a[i] = base;
+        quicksort(a,lo,i-1);
+        quicksort(a,i+1,hi);
+    }
+    public static void main(String[] args){
+        int[] a = new int[]{7,3,9,1,0,3,6,10,2,4};
+        quicksort(a,0,a.length-1);
+        for(int n : a){
+            System.out.print(n + " ");
         }
     }
-
+}
